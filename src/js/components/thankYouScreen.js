3e6f5e0419f9f505b8f888b9ee7c4211d31dbe2a
Kromus <81479242+Kromus22@users.noreply.github.com>
2023-04-26 07:41:06 +0300

    Delete src directory

--- src/js/components/thankYouScreen.js
+++
@@ -1,8 +0,0 @@
-export const createThankYouScreen = (item) => {
-  const container = document.querySelector('.form-wrapper');
-  container.textContent = '';
-
-  container.innerHTML = `
-  <p>Спасибо ${item.name}! Мы вышлем результаты на Вашу почту - ${item.email}</p>
-  `;
-}
--- src/js/helper/createElement.js
+++
@@ -1,4 +0,0 @@
-export const createElement = (tag, attr) => {
-  const element = document.createElement(tag);
-  return Object.assign(element, attr);
-}
